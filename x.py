125bf52598b3c3b4d06c9e35ca5784edb22e1695
balram tondare <80520558+balram20@users.noreply.github.com>
2023-03-26 02:07:15 +0530

    Add files via upload

---
+++ x.py
@@ -0,0 +1,7 @@
+import socket
+hostname = input("Please enter website address:\n")
+# IP lookup from hostname
+print(f'The {hostname} IP Address is {socket.gethostbyname(hostname)}')
+
+
+# print("\n"hostname.lookup())
